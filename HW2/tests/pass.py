2a55157d305f8756c73295cdf09c8c1a83c64442
MIN TING TU <79685521+katydu@users.noreply.github.com>
2024-09-05 19:34:22 +0000

    add second test case

--- HW2/tests/pass.py
+++ HW2/tests/pass.py
@@ -4,3 +4,8 @@ def test_single_element_array():
     arr = [1]
     sorted_array = mergeSort(arr)
     assert sorted_array == arr
+
+def test_normal_array():
+    arr = [2,5,3,8,9,6]
+    sort_array = mergeSort(arr)
+    assert sort_array == [2,3,5,6,8,9]

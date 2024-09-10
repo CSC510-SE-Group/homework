687d73512f353756a4c4bbdbd397d0d438eda053
SiddhiKhairee <siddhi.k612@gmail.com>
2024-09-10 17:48:10 +0000

    added static tools and new test case

--- HW2/tests/merge-testcases.py
+++ HW2/tests/merge-testcases.py
@@ -11,3 +11,8 @@ def test_normal_array():
     arr = [2, 5, 3, 8, 9, 6]
     sort_array = mergeSort(arr)
     assert sort_array == [2, 3, 5, 6, 8, 9]
+
+def test_same_elements():
+    arr = [3, 3, 3, 3, 3, 3]   
+    sortedd_array = mergeSort(arr)
+    assert sortedd_array == arr

b1493ed265478d07b58b9217cc41a3418c83261b
SiddhiKhairee <siddhi.k612@gmail.com>
2024-10-10 13:21:47 +0000

    Your commit message here

--- HW4/task3.sh
+++ HW4/task3.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-
-# Extract passengers from 2nd class who embarked at Southampton,
-# replace male/female labels with M/F, and calculate the average age.
-
 awk -F',' '
 NR == 1 {
     print; 
@@ -16,13 +11,13 @@ NR > 1 {
     gsub(/[^a-zA-Z]/, "", $12); 
     if ($12 == "S" && $3 == "2") {
         print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "," $7 "," $8 "," $9 "," $10 "," $11 "," $12;
-        total_age += $6; # Add age to total_age
-        count++;         # Increment count of passengers
+        total_age += $6; 
+        count++;         
     } 
 } 
 END {
     if (count > 0) {
-        avg_age = total_age / count;  # Calculate average age
+        avg_age = total_age / count;  
         print "Average Age of Filtered Passengers: " avg_age;
     } else {
         print "No filtered passengers found.";
